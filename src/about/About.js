87df8aa1b66ce32c6f1eea92f0a5d4f9af1847c3
rajan-gith <rajan.prajapati@berylsystems.com>
2019-11-02 18:54:38 +0530

    about component files added

---
+++ src/about/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './About.css'
+
+const About = (argument) => {
+	return (
+		<div className="about-container">
+			<h2>Welcome to about us page.</h2>
+		</div>
+	)
+}
+
+export default About;
