e94e8e012871afa4678e19e11c661e941b9f89ca
rajan-gith <rajan.prajapati@berylsystems.com>
2019-11-02 18:55:44 +0530

    components integrated

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Menu from './menu/Menu.js';
+import About from './about/About.js';
+import Contact from './contact/Contact.js';
+import './menu/Menu.css'
+import
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="App">
+        <h1>Welcome to react routing</h1>
+      </div>
+      <div className="menu-container">
+        <Menu/>
+        <Home/>
+        <About/>
+        <Contact/>
+      </div>
     </div>
   );
+}
+
+const Home = (argument) => {
+  return (
+    <div className="home-container">
+      <h2>Welcome to Home page.</h2>
+    </div>
+  )
 }
 
 export default App;
