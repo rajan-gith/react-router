ee334ec729a8b53623de35bcad295128b4d1fef6
rajan-gith <rajan.prajapati@berylsystems.com>
2019-11-02 18:55:00 +0530

    contact component file added

---
+++ src/contact/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Contact.css'
+
+const Contact = (argument) => {
+	return (
+		<div className="contact-container">
+			<h2>Welcome to Contact us page.</h2>
+		</div>
+	)
+}
+
+export default Contact;
