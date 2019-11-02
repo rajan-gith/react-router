7158127087a78f90c6735ef1546054ca7e1e7de1
rajan-gith <rajan.prajapati@berylsystems.com>
2019-11-02 18:55:26 +0530

    menu cmpt files added

---
+++ src/menu/Menu.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Menu.css'
+
+const Menu = (argument) => {
+	return (
+		<div>
+			<ul className="navstyle">
+				<li>
+					Home
+				</li>
+				<li>
+					About
+				</li>
+				<li>
+					Contact
+				</li>
+			</ul>
+		</div>
+	)
+}
+
+export default Menu;
